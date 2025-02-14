2411a1d82c065a3f5cf72e05cc7952af575b9ee2
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-14 18:05:16 -0500

    feat(identity): Creation of the Identity class with its constructors and getValue

--- src/shared/src/domain/src/main/java/com/buildingblocks/shared/domain/generic/Identity.java
+++ src/shared/src/domain/src/main/java/com/buildingblocks/shared/domain/generic/Identity.java
@@ -1,4 +1,17 @@
 package com.buildingblocks.shared.domain.generic;
 
-public class Identity {
+import java.util.UUID;
+
+public abstract class Identity {
+  private final String value;
+
+  protected Identity() {
+    this.value = UUID.randomUUID().toString();
+  }
+  protected Identity (final String value){
+    this.value = value;
+  }
+  public String getValue(){
+    return value;
+  }
 }
