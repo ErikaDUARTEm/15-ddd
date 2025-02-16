4974848ce7e78ea2d8a2b5028316586f0474df8c
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 11:50:07 -0500

    feat(events): add properties to event to advancebox

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/AdvancedBox.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/AdvancedBox.java
@@ -3,7 +3,32 @@ package com.buildingblocks.movementsandtactics.domain.movements.events;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class AdvancedBox extends DomainEvent {
-  public AdvancedBox() {
+  private final Integer row;
+  private final Integer column;
+  private final Integer piece;
+  private final Integer idPlayer;
+
+  public AdvancedBox(Integer row, Integer column, Integer piece, Integer idPlayer) {
     super(EventsEnum.ADVANCED_BOX.name());
+    this.row = row;
+    this.column = column;
+    this.piece = piece;
+    this.idPlayer = idPlayer;
+  }
+
+  public Integer getRow() {
+    return row;
+  }
+
+  public Integer getColumn() {
+    return column;
+  }
+
+  public Integer getIdPlayer() {
+    return idPlayer;
+  }
+
+  public Integer getPiece() {
+    return piece;
   }
 }
