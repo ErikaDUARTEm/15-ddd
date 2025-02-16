94f839c71bc5f3375ada01b216dec3dceb2f41bd
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 15:20:29 -0500

    feat(events): add properties to event to validatedPieceType

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/ValidatedPieceType.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/ValidatedPieceType.java
@@ -3,7 +3,26 @@ package com.buildingblocks.movementsandtactics.domain.movements.events;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class ValidatedPieceType extends DomainEvent {
-  public ValidatedPieceType() {
+  private final Integer idPiece;
+  private final String expectedType;
+  private final Boolean isValid;
+
+  public ValidatedPieceType(Integer idPiece, String expectedType, Boolean isValid) {
     super(EventsEnum.VALIDATED_PIECE_TYPE.name());
+    this.idPiece = idPiece;
+    this.expectedType = expectedType;
+    this.isValid = isValid;
+  }
+
+  public Integer getIdPiece() {
+    return idPiece;
+  }
+
+  public String getExpectedType() {
+    return expectedType;
+  }
+
+  public Boolean getValid() {
+    return isValid;
   }
 }
