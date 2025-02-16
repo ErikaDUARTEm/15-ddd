21a810ea253cc3c08e26b145204d0fdf215e0a69
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 13:19:29 -0500

    feat(events): add properties to event to assignedShift

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/AssignedShift.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/AssignedShift.java
@@ -3,7 +3,20 @@ package com.buildingblocks.movementsandtactics.domain.movements.events;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class AssignedShift extends DomainEvent {
-  public AssignedShift() {
+  private final String idPlayer;
+  private final String currentShift;
+
+  public AssignedShift(String idPlayer, String currentShift) {
     super(EventsEnum.ASSIGNED_SHIFT.name());
+    this.idPlayer = idPlayer;
+    this.currentShift = currentShift;
+  }
+
+  public String getIdPlayer() {
+    return idPlayer;
+  }
+
+  public String getCurrentShift() {
+    return currentShift;
   }
 }
