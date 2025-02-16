df7b44bfe4493903187b9bc5703b3f503aadec37
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 15:12:28 -0500

    feat(events): add properties to event to ExecutedMovement

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/ExecutedMovement.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/ExecutedMovement.java
@@ -1,9 +1,41 @@
 package com.buildingblocks.movementsandtactics.domain.movements.events;
 
+import com.buildingblocks.movementsandtactics.domain.movements.values.PositionPiece;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class ExecutedMovement extends DomainEvent {
-  public ExecutedMovement() {
+  private final Integer idMovement;
+  private final Integer idPlayer;
+  private final Integer idPiece;
+  private final PositionPiece positionInitial;
+  private final PositionPiece positionFinal;
+
+  public ExecutedMovement(Integer idMovement, Integer idPlayer, Integer idPiece, PositionPiece positionInitial, PositionPiece positionFinal) {
     super(EventsEnum.EXECUTED_MOVEMENT.name());
+    this.idMovement = idMovement;
+    this.idPlayer = idPlayer;
+    this.idPiece = idPiece;
+    this.positionInitial = positionInitial;
+    this.positionFinal = positionFinal;
+  }
+
+  public Integer getIdMovement() {
+    return idMovement;
+  }
+
+  public Integer getIdPlayer() {
+    return idPlayer;
+  }
+
+  public Integer getIdPiece() {
+    return idPiece;
+  }
+
+  public PositionPiece getPositionInitial() {
+    return positionInitial;
+  }
+
+  public PositionPiece getPositionFinal() {
+    return positionFinal;
   }
 }
