2c1170169145fbff6941cade8b275ed4b7473607
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 18:22:58 -0500

    feat(values): HistoryMovements

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/values/HistoryMovements.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/values/HistoryMovements.java
@@ -1,4 +1,29 @@
 package com.buildingblocks.movementsandtactics.domain.movements.values;
 
-public class HistoryMovements {
+import com.buildingblocks.domain.shared.domain.generic.IValueObject;
+import com.buildingblocks.movementsandtactics.domain.movements.Movement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryMovements implements IValueObject {
+  private final List<Movement> movements;
+
+  private HistoryMovements(final List<Movement> movements) {
+    this.movements = new ArrayList<>(movements);
+    validate();
+  }
+  public static HistoryMovements of(List<Movement> movements){
+    return new HistoryMovements(movements);
+  }
+  @Override
+  public void validate() {
+    if (movements == null) {
+      throw new IllegalArgumentException("Movements cannot be null");
+    }
+  }
+
+  public List<Movement> getMovements() {
+    return movements;
+  }
 }
