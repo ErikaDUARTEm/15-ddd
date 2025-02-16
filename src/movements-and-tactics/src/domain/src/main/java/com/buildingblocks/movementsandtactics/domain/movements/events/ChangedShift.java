ef8149567eb3b8003fa212e662f1d4212c20328a
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 15:30:14 -0500

    feat(events): add properties to event to changedShift

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/ChangedShift.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/ChangedShift.java
@@ -3,7 +3,20 @@ package com.buildingblocks.movementsandtactics.domain.movements.events;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class ChangedShift extends DomainEvent {
-  public ChangedShift() {
+  private final Integer idPreviousPlayer;
+  private final Integer idNewPlayer;
+
+  public ChangedShift(Integer idPreviousPlayer, Integer idNewPlayer) {
     super(EventsEnum.CHANGED_SHIFT.name());
+    this.idPreviousPlayer = idPreviousPlayer;
+    this.idNewPlayer = idNewPlayer;
+  }
+
+  public Integer getIdPreviousPlayer() {
+    return idPreviousPlayer;
+  }
+
+  public Integer getIdNewPlayer() {
+    return idNewPlayer;
   }
 }
