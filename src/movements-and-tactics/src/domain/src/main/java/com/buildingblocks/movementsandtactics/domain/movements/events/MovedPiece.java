ac02fc057b1af112ca3b580335d9bcc9b84a1350
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 15:24:16 -0500

    feat(events): add properties to event to movedPiece

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/MovedPiece.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/MovedPiece.java
@@ -1,9 +1,35 @@
 package com.buildingblocks.movementsandtactics.domain.movements.events;
 
+import com.buildingblocks.movementsandtactics.domain.movements.values.PositionPiece;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class MovedPiece extends DomainEvent {
-  public MovedPiece() {
+  private final Integer idPlayer;
+  private final Integer idPiece;
+  private final PositionPiece positionInitial;
+  private final PositionPiece positionFinal;
+
+  public MovedPiece(Integer idPlayer, Integer idPiece, PositionPiece positionInitial, PositionPiece positionFinal) {
     super(EventsEnum.MOVED_PIECE.name());
+    this.idPlayer = idPlayer;
+    this.idPiece = idPiece;
+    this.positionInitial = positionInitial;
+    this.positionFinal = positionFinal;
+  }
+
+  public Integer getIdPlayer() {
+    return idPlayer;
+  }
+
+  public Integer getIdPiece() {
+    return idPiece;
+  }
+
+  public PositionPiece getPositionInitial() {
+    return positionInitial;
+  }
+
+  public PositionPiece getPositionFinal() {
+    return positionFinal;
   }
 }
