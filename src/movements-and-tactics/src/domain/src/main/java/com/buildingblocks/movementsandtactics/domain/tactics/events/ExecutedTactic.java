c810794c218b5e2cdbac100081cddab3d5004f0f
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 15:48:38 -0500

    feat(events): add properties to event to executedTactic

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/tactics/events/ExecutedTactic.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/tactics/events/ExecutedTactic.java
@@ -3,7 +3,26 @@ package com.buildingblocks.movementsandtactics.domain.tactics.events;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class ExecutedTactic extends DomainEvent {
-  public ExecutedTactic() {
+  private final Integer idTactic;
+  private final Integer idPlayer;
+  private final String description;
+
+  public ExecutedTactic(Integer idTactic, Integer idPlayer, String description) {
     super(EventsEnum.EXECUTED_TACTIC.name());
+    this.idTactic = idTactic;
+    this.idPlayer = idPlayer;
+    this.description = description;
+  }
+
+  public Integer getIdTactic() {
+    return idTactic;
+  }
+
+  public Integer getIdPlayer() {
+    return idPlayer;
+  }
+
+  public String getDescription() {
+    return description;
   }
 }
