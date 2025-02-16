167af4366056ce79839be2b6afeec026e3963fdf
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 15:51:21 -0500

    feat(events): add properties to event to ValidatedTactic

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/tactics/events/ValidatedTactic.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/tactics/events/ValidatedTactic.java
@@ -3,7 +3,32 @@ package com.buildingblocks.movementsandtactics.domain.tactics.events;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class ValidatedTactic extends DomainEvent {
-  public ValidatedTactic() {
+  private final Integer idTactic;
+  private final Integer idPlayer;
+  private final String description;
+  private final Boolean isValid;
+
+  public ValidatedTactic(Integer idTactic, Integer idPlayer, String description, Boolean isValid) {
     super(EventsEnum.VALIDATED_TACTIC.name());
+    this.idTactic = idTactic;
+    this.idPlayer = idPlayer;
+    this.description = description;
+    this.isValid = isValid;
+  }
+
+  public Integer getIdTactic() {
+    return idTactic;
+  }
+
+  public Integer getIdPlayer() {
+    return idPlayer;
+  }
+
+  public Boolean getValid() {
+    return isValid;
+  }
+
+  public String getDescription() {
+    return description;
   }
 }
