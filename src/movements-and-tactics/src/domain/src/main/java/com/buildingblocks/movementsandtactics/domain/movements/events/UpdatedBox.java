4cd9185e2ffb5b6c8a6db484169af998ee909c5e
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 15:35:34 -0500

    feat(events): add properties to event to updatedBox

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/UpdatedBox.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/UpdatedBox.java
@@ -1,9 +1,30 @@
 package com.buildingblocks.movementsandtactics.domain.movements.events;
 
+import com.buildingblocks.movementsandtactics.domain.movements.entities.PieceMovement;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class UpdatedBox extends DomainEvent {
-  public UpdatedBox() {
+  private final Integer row;
+  private final Integer column;
+  private final PieceMovement piece;
+
+
+  public UpdatedBox(Integer row, Integer column, PieceMovement piece) {
     super(EventsEnum.UPDATED_BOX.name());
+    this.row = row;
+    this.column = column;
+    this.piece = piece;
+  }
+
+  public Integer getRow() {
+    return row;
+  }
+
+  public Integer getColumn() {
+    return column;
+  }
+
+  public PieceMovement getPiece() {
+    return piece;
   }
 }
