5fcfa7cce1d66c2e1fc57183251068d61c5dc41b
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 15:43:30 -0500

    feat(events): add properties to event to updatedMovement

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/UpdatedMovement.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/events/UpdatedMovement.java
@@ -1,9 +1,49 @@
 package com.buildingblocks.movementsandtactics.domain.movements.events;
 
+import com.buildingblocks.movementsandtactics.domain.movements.values.PieceColor;
+import com.buildingblocks.movementsandtactics.domain.movements.values.PieceType;
+import com.buildingblocks.movementsandtactics.domain.movements.values.PositionPiece;
 import com.buildingblocks.movementsandtactics.domain.shared.domain.generic.DomainEvent;
 
 public class UpdatedMovement extends DomainEvent {
-  public UpdatedMovement() {
+  private final Integer idMovement;
+  private final Integer idPlayer;
+  private final PositionPiece positionInitial;
+  private final PositionPiece positionFinal;
+  private final PieceType pieceType;
+  private final PieceColor pieceColor;
+
+  public UpdatedMovement(Integer idMovement, Integer idPlayer, PositionPiece positionInitial, PositionPiece positionFinal, PieceType pieceType, PieceColor pieceColor) {
     super(EventsEnum.UPDATED_MOVEMENT.name());
+    this.idMovement = idMovement;
+    this.idPlayer = idPlayer;
+    this.positionInitial = positionInitial;
+    this.positionFinal = positionFinal;
+    this.pieceType = pieceType;
+    this.pieceColor = pieceColor;
+  }
+
+  public Integer getIdMovement() {
+    return idMovement;
+  }
+
+  public Integer getIdPlayer() {
+    return idPlayer;
+  }
+
+  public PositionPiece getPositionInitial() {
+    return positionInitial;
+  }
+
+  public PositionPiece getPositionFinal() {
+    return positionFinal;
+  }
+
+  public PieceType getPieceType() {
+    return pieceType;
+  }
+
+  public PieceColor getPieceColor() {
+    return pieceColor;
   }
 }
