6ad3d67e7fe660a83d5a0e35e795ad202e685a18
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 22:30:49 -0500

    feat(values): conditions

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/tactics/values/GeneralConditions.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/tactics/values/GeneralConditions.java
@@ -1,4 +1,30 @@
 package com.buildingblocks.movementsandtactics.domain.tactics.values;
 
-public class GeneralConditions {
+import com.buildingblocks.domain.shared.domain.generic.IValueObject;
+
+import java.util.List;
+
+import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotEmpty;
+import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotNull;
+
+public class GeneralConditions implements IValueObject {
+  private final List<String> conditions;
+
+  private GeneralConditions(final List<String> conditions) {
+    this.conditions = conditions;
+  }
+  public static GeneralConditions of(List<String> conditions){
+    return new GeneralConditions(conditions);
+  }
+  @Override
+  public void validate() {
+    for (String condition : conditions) {
+      validateNotEmpty(condition);
+      validateNotNull(condition);
+    }
+  }
+
+  public List<String> getConditions() {
+    return conditions;
+  }
 }
--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/tactics/values/SpecificConditions.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/tactics/values/SpecificConditions.java
@@ -1,4 +1,31 @@
 package com.buildingblocks.movementsandtactics.domain.tactics.values;
 
-public class SpecificConditions {
+import com.buildingblocks.domain.shared.domain.generic.IValueObject;
+
+import java.util.List;
+
+import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotEmpty;
+import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotNull;
+
+public class SpecificConditions implements IValueObject {
+  private final List<String> conditions;
+
+  private SpecificConditions(final List<String> conditions) {
+    this.conditions = conditions;
+  }
+  public static SpecificConditions of(List<String> conditions){
+    return new SpecificConditions(conditions);
+  }
+  @Override
+  public void validate() {
+    for (String condition : conditions) {
+       validateNotEmpty(condition);
+       validateNotNull(condition);
+    }
+  }
+
+  public List<String> getConditions() {
+    return conditions;
+  }
+
 }
