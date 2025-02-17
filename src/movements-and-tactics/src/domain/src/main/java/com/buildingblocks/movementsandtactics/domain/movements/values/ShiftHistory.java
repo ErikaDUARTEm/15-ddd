2c2e0ff57aab01e72be9bbff5265124217cae1e5
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 21:01:52 -0500

    feat(values): shiftHistory

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/values/ShiftHistory.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/values/ShiftHistory.java
@@ -1,4 +1,33 @@
 package com.buildingblocks.movementsandtactics.domain.movements.values;
 
-public class ShiftHistory {
+import com.buildingblocks.domain.shared.domain.generic.IValueObject;
+import com.buildingblocks.movementsandtactics.domain.movements.entities.Shift;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotEmpty;
+import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotNull;
+
+public class ShiftHistory implements IValueObject {
+  private final List<Shift> shifts;
+
+  private ShiftHistory(List<Shift> shifts) {
+    this.shifts = new ArrayList<>(shifts);
+    validate();
+  }
+  public static ShiftHistory of(List<Shift> shifts){
+    return new ShiftHistory(shifts);
+  }
+  @Override
+  public void validate() {
+    for (Shift shift : shifts) {
+      validateNotNull(shift.toString());
+      validateNotEmpty(shift.toString());
+    }
+  }
+
+  public List<Shift> getShifts() {
+    return shifts;
+  }
 }
