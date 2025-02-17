130aef8c7dd968becaa9e9dfe0033bd4013abd45
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-16 23:59:17 -0500

    feat(entity): assign and record shift

--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/entities/Shift.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/entities/Shift.java
@@ -1,4 +1,71 @@
 package com.buildingblocks.movementsandtactics.domain.movements.entities;
 
-public class Shift {
+import com.buildingblocks.domain.shared.domain.generic.Entity;
+import com.buildingblocks.movementsandtactics.domain.movements.values.CurrentShift;
+import com.buildingblocks.movementsandtactics.domain.movements.values.ShiftHistory;
+import com.buildingblocks.movementsandtactics.domain.movements.values.ShiftId;
+
+import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotEmpty;
+import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotNull;
+
+public class Shift extends Entity<ShiftId> {
+  private CurrentShift currentShift;
+  private ShiftHistory history;
+  //region Constructors
+  public Shift(ShiftId identity, CurrentShift currentShift, ShiftHistory history) {
+    super(identity);
+    this.currentShift = currentShift;
+    this.history = history;
+  }
+  public Shift(CurrentShift currentShift, ShiftHistory history) {
+    super(new ShiftId());
+    this.currentShift = currentShift;
+    this.history = history;
+  }
+ //endregion
+  //region Getters and Setters
+  public CurrentShift getCurrentShift() {
+    return currentShift;
+  }
+
+  public void setCurrentShift(CurrentShift currentShift) {
+    this.currentShift = currentShift;
+  }
+
+  public ShiftHistory getHistory() {
+    return history;
+  }
+
+  public void setHistory(ShiftHistory history) {
+    this.history = history;
+  }
+  //endregion
+  //region Methods
+   public void assign(String idPlayer, Integer numberShift) {
+    validateNotEmpty(idPlayer);
+    validateNotNull(idPlayer);
+    validateNotEmpty(String.valueOf(numberShift));
+    validateNotNull(String.valueOf(numberShift));
+    if (currentShift == null) {
+      currentShift = CurrentShift.of(numberShift);
+      return;
+    }
+    if (currentShift.getNumberShift().equals(numberShift)) {
+      return;
+    }
+    currentShift = CurrentShift.of(numberShift);
+  }
+
+  public void record(){
+    validateNotNull(currentShift.toString());
+    history.addShift(currentShift);
+    currentShift = null;
+  }
+  public void change(String idPlayerNew, Integer numberShiftNew) {
+    if(currentShift != null){
+      record();
+    }
+    assign(idPlayerNew, numberShiftNew);
+  }
+  //endregion
 }
--- src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/values/ShiftHistory.java
+++ src/movements-and-tactics/src/domain/src/main/java/com/buildingblocks/movementsandtactics/domain/movements/values/ShiftHistory.java
@@ -4,30 +4,34 @@ import com.buildingblocks.domain.shared.domain.generic.IValueObject;
 import com.buildingblocks.movementsandtactics.domain.movements.entities.Shift;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotEmpty;
 import static com.buildingblocks.domain.shared.domain.utils.Validate.validateNotNull;
 
 public class ShiftHistory implements IValueObject {
-  private final List<Shift> shifts;
+  private final List<CurrentShift> shifts;
 
-  private ShiftHistory(List<Shift> shifts) {
+  private ShiftHistory(List<CurrentShift> shifts) {
     this.shifts = new ArrayList<>(shifts);
     validate();
   }
-  public static ShiftHistory of(List<Shift> shifts){
+  public static ShiftHistory of(List<CurrentShift> shifts){
     return new ShiftHistory(shifts);
   }
   @Override
   public void validate() {
-    for (Shift shift : shifts) {
+    for (CurrentShift shift : shifts) {
       validateNotNull(shift.toString());
       validateNotEmpty(shift.toString());
     }
   }
+  public void addShift(CurrentShift newShift) {
+    shifts.add(newShift);
+  }
 
-  public List<Shift> getShifts() {
+  public List<CurrentShift> getShifts() {
     return shifts;
   }
 }
