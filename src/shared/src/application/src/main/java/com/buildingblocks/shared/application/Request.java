93d05242c3adc3a22ac592a783199905bc8aa51d
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-23 14:01:36 -0500

    feat(application):add class Request

--- src/shared/src/application/src/main/java/com/buildingblocks/shared/application/Request.java
+++ src/shared/src/application/src/main/java/com/buildingblocks/shared/application/Request.java
@@ -1,4 +1,13 @@
 package com.buildingblocks.shared.application;
 
-public class Request {
+public abstract class Request {
+  private final String aggregateId;
+
+  protected Request(String aggregateId) {
+    this.aggregateId = aggregateId;
+  }
+
+  public String getAggregateId() {
+    return aggregateId;
+  }
 }
